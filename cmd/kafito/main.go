a1ce808dca99f346b861ec30621a85cfbe0bc78d
MahdiRazaqi <MahdiRazaqi77@gmail.com>
2022-01-18 18:07:32 +0330

    init project into main.go

---
+++ cmd/kafito/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/trykafito/kafito/config"
+	"github.com/trykafito/kafito/internal/server"
+	"github.com/trykafito/kafito/pkg/database"
+	"github.com/trykafito/kafito/pkg/logger"
+)
+
+var configPath = flag.String("c", "./config.json", "config file path")
+
+func main() {
+	flag.Parse()
+
+	c, err := config.Read(*configPath)
+	if err != nil {
+		logger.Panic(err)
+	}
+
+	if err := database.Connect(c.Mongo.Host, c.Mongo.DB, c.Mongo.User, c.Mongo.Password); err != nil {
+		logger.Panic(err)
+	}
+
+	if err := server.Start(c.Port, c.SecretKey); err != nil {
+		logger.Panic(err)
+	}
+}
